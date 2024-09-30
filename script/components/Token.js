34e491064d86f7f1eb64298614dc36c0c3a72264
chongwei-fsd <ChongWei@genstudents.org>
2024-09-30 09:59:30 +0800

    latest version

---
+++ script/components/Token.js
@@ -0,0 +1,24 @@
+class Token{
+    constructor(token){
+        this.token=token
+    }
+    isValidProfileUrl(){
+        if(!this.token) window.location = _LOGIN_URL
+        
+        if(this.token){
+            const user = decodeUser(this.token);
+            const profileEmail = document.getElementById("useremail");
+            const profileUsername = document.getElementById("username");
+            profileUsername.classList.add("fw-bold");
+            profileEmail.innerText = user.email;
+            profileUsername.innerText = user.uname;
+        }    
+    }
+    // isValidLoginUrl(){
+    //     if(this.token) window.location = _PROFILE_URL
+    // }
+    // isValidRegisterUrl(){
+    //     if(this.token) window.location = _PROFILE_URL
+    // }
+
+}
